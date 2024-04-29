2f67ac9fb05e467646d2ee9090c7683dea6db137
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-04-30 00:32:54 +0200

    api loadGrid documentée

--- src/pages/api/loadGrid/[id].js
+++ src/pages/api/loadGrid/[id].js
@@ -1,27 +1,31 @@
+// Ce fichier définit un gestionnaire d'API pour récupérer les données d'une grille.
+
+// Importation de la fonction de connexion à la base de données MongoDB.
 import { connectToDatabase } from '../../../../lib/utils/mongodb/db.js';
 
+// Le gestionnaire de requêtes asynchrones pour les requêtes GET vers '/api/grid/{id}'.
 export default async function handler(req, res) {
+    // Récupération de l'ID de grille depuis la requête.
     const { id } = req.query;
+
     try {
+        // Connexion à la base de données MongoDB.
         const { db } = await connectToDatabase();
+
+        // Recherche de la grille par son identifiant.
         const grid = await db.collection("grids").findOne({ gridId: id });
 
+        // Si la grille n'est pas trouvée, renvoie une erreur 404.
         if (!grid) {
             console.log("No grid found for ID:", id);
             return res.status(404).json({ message: 'Grid not found' });
         }
 
-        const timeCreated = grid.createdAt;
-        const timeNow = new Date();
-        const threeHours = 3 * 60 * 60 * 1000; // 3 heures en millisecondes tset
-        const isLocked = timeNow - new Date(timeCreated) > threeHours;
-
-        res.status(200).json({ ...grid, isLocked });
+        // Si la grille est trouvée, renvoie les données de la grille avec un statut 200.
+        res.status(200).json(grid);
     } catch (error) {
+        // En cas d'erreur pendant la requête à la base de données, renvoie une erreur 500.
         console.error("Error loading grid:", error);
         res.status(500).json({ message: 'Failed to load grid', error: error.message });
     }
 }
-
-
-
