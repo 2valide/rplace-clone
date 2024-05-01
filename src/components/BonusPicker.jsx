d3518a5ebcaffe454c4280e5358e75161f2ea2fb
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-05-01 13:40:22 +0200

    responsive

--- src/components/BonusPicker.jsx
+++ src/components/BonusPicker.jsx
@@ -4,7 +4,7 @@ import BonusLineX from "./BonusLineX";
 
 export default function BonusPicker({ onBonusSelect }) {
   return (
-    <div className="bg-UtilitiesBackground p-4 flex flex-col gap-4 mr-32 rounded-xl">
+    <div className="bg-UtilitiesBackground p-4 flex flex-row 2xl:flex-col gap-4 2xl:mr-32 rounded-xl">
       <BonusGrenade type="bomb" onActivate={() => onBonusSelect("bomb")} />
       <BonusLine type="line" onActivate={() => onBonusSelect("line")} />
       <BonusLineX type="lineX" onActivate={() => onBonusSelect("lineX")} />
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,7 @@ export default function MainContent() {
   return (
     <>
       <div className="mb-3.5 mt-8">
-        <div className=" bg-MainContentBackground rounded-3xl flex items-center justify-center gap-5 flex-row px-36 py-10">
+        <div className="bg-MainContentBackground rounded-3xl md:flex items-center justify-center gap-5 flex-row flex-wrap px-0 py-0 lg:px-36 lg:py-10 hidden ">
           <WarsArea />
         </div>
       </div>
